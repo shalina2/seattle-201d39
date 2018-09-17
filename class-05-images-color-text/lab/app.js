0ee7dcbe142deda7db309c0f7a54830ab78ceb98
shalom belaineh <shalom.belaineh@gmail.com>
2018-09-17 01:26:26 -0700

    sum function

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -9,11 +9,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    var sum = num1 + num2;
+    var summary = num1 + '+' + num2 + '=' + sum;
+    return[sum,summary];
 }
-
+testSum(4,7);
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
